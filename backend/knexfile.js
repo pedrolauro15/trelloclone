e63d493adbcdc5d2de44cff5f5e5eb9251c3f79a
pedrolauro15 <pedrolauro15@gmail.com>
2020-05-17 15:46:20 -0300

    backend finish

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -16,13 +16,10 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'dhsi2hl4d99e',
-      user:     'zivgfmibdgeyhw',
-      password: 'b0d9a8e32016e49c3a9bbcabeca9cd315237192154ce597668151e8c5dbdab96'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: "ec2-34-230-149-169.compute-1.amazonaws.com",
+      user : 'zivgfmibdgeyhw',
+      password : 'b0d9a8e32016e49c3a9bbcabeca9cd315237192154ce597668151e8c5dbdab96',
+      database : 'dhsi2hl4d99e'
     },
     migrations: {
       tableName: 'knex_migrations',
